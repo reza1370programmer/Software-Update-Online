d21a087683ecac9fb3589a6d9e604cd48f7b2e7f
reza1370programmer <78476427+reza1370programmer@users.noreply.github.com>
2024-12-23 19:45:59 +0330

    Add project files.

---
+++ Update/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Update
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblNewVer = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCurentVer = new System.Windows.Forms.Label();
+            this.lblCheck = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(298, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ذخیره";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblNewVer
+            // 
+            this.lblNewVer.Location = new System.Drawing.Point(253, 34);
+            this.lblNewVer.Name = "lblNewVer";
+            this.lblNewVer.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblNewVer.Size = new System.Drawing.Size(74, 21);
+            this.lblNewVer.TabIndex = 17;
+            this.lblNewVer.Text = "|";
+            this.lblNewVer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCount
+            // 
+            this.lblCount.Location = new System.Drawing.Point(5, 95);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblCount.Size = new System.Drawing.Size(64, 20);
+            this.lblCount.TabIndex = 16;
+            this.lblCount.Text = "|";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(330, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "نسخه جدید:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(328, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "نسخه جاری:";
+            // 
+            // lblCurentVer
+            // 
+            this.lblCurentVer.Location = new System.Drawing.Point(253, 5);
+            this.lblCurentVer.Name = "lblCurentVer";
+            this.lblCurentVer.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblCurentVer.Size = new System.Drawing.Size(74, 21);
+            this.lblCurentVer.TabIndex = 13;
+            this.lblCurentVer.Text = "|";
+            this.lblCurentVer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCheck
+            // 
+            this.lblCheck.Location = new System.Drawing.Point(56, 95);
+            this.lblCheck.Name = "lblCheck";
+            this.lblCheck.Size = new System.Drawing.Size(338, 45);
+            this.lblCheck.TabIndex = 12;
+            this.lblCheck.Text = "|";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(187, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(207, 26);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "بروزرسانی";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 228);
+            this.Controls.Add(this.lblNewVer);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCurentVer);
+            this.Controls.Add(this.lblCheck);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblNewVer;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCurentVer;
+        private System.Windows.Forms.Label lblCheck;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Update/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Compression;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+using System.Data.SqlClient;
+
+namespace Update
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int _countUpdate;
+        int _count = 0;
+        public bool CheckInternetConnection()
+        {
+            try
+            {
+                bool check = true;
+                string[] sitesList = new string[3] { "www.google.com", "www.microsoft.com", "www.yahoo.com" };
+                Ping ping = new Ping();
+                PingReply reply;
+                int count = 0;
+                for (int i = 0; i < sitesList.Length; i++)
+                {
+                    reply = ping.Send(sitesList[i]);
+                    if (reply.Status == IPStatus.Success)
+                        count += 1;
+                }
+                if (count > 0)
+                    check = true;
+                else
+                    check = false;
+                return check;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        private void UpdateApp()
+        {
+            //خارج کردن از حالت فشرده
+            string[] subFileEntries = Directory.GetFiles(Path.GetTempPath() + "\\Dotnetyar");
+            foreach (string file in subFileEntries)
+                ZipFile.ExtractToDirectory(file, Path.GetTempPath() + "\\Dotnetyar");
+
+
+
+            //جدا کردن فایل های نصبی از فایل برنامه
+            if (!Directory.Exists(Path.GetTempPath() + "\\Dotnetyar\\Install"))
+            {
+                Directory.CreateDirectory(Path.GetTempPath() + "\\Dotnetyar\\Install");
+            }
+
+            string[] subdirectoryEntries = Directory.GetDirectories(Path.GetTempPath() + "\\Dotnetyar");
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                if (Directory.Exists(subdirectory + "\\Install"))
+                {
+                    string[] subFile = Directory.GetFiles(subdirectory + "\\Install");
+                    foreach (string file in subFile)
+                    {
+                        int count = 1;
+                        string fileNameOnly = Path.GetFileNameWithoutExtension(file);
+                        string extension = Path.GetExtension(file);
+                        string newFullPath = Path.GetTempPath() + "\\Dotnetyar\\Install\\" + fileNameOnly + extension;
+                        while (File.Exists(newFullPath))
+                        {
+                            string tempFileName = fileNameOnly + "(" + count++ + ")";
+                            newFullPath = Path.GetTempPath() + "\\Dotnetyar\\Install\\" + tempFileName + extension;
+                        }
+
+                        File.Move(file, newFullPath);
+                    }
+                    Directory.Delete(subdirectory + "\\Install");
+                }
+            }
+
+
+
+            //کپی فایل های برنامه کنار نرم افزار
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+
+                if (!subdirectory.Contains("Install"))
+                {
+                    string sourceFolder = subdirectory;
+                    string updateFolder = Application.StartupPath;
+                    string[] originalFiles = Directory.GetFiles(sourceFolder, "*", SearchOption.AllDirectories);
+                    Array.ForEach(originalFiles, (originalFileLocation) =>
+                    {
+                        FileInfo originalFile = new FileInfo(originalFileLocation);
+                        FileInfo destFile = new FileInfo(originalFileLocation.Replace(sourceFolder, updateFolder));
+                        if (destFile.Exists)
+                        {
+                            originalFile.CopyTo(destFile.FullName, true);
+                        }
+                        else
+                        {
+                            Directory.CreateDirectory(destFile.DirectoryName);
+                            originalFile.CopyTo(destFile.FullName, false);
+                        }
+                    });
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (CheckInternetConnection())
+            {
+                var versionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(Application.StartupPath + "\\App.exe");
+                string currentVer = versionInfo.ProductVersion;
+
+                string xmlUrl = "http://dl.dotnetyar.com/Update/update.xml";
+                XDocument xml = XDocument.Load(xmlUrl);
+                string newVer = string.Empty;
+
+
+                foreach (XElement element in xml.Descendants("Version"))
+                {
+                    newVer = element.Value.ToString();
+                }
+
+                if (currentVer.Trim() == newVer.Trim())
+                    lblCheck.Text = "نسخه جدید موجود نیست";
+                else
+                {
+                    button2.Enabled = false;
+                    lblCheck.Text = "نسخه جدید موجود است،در حال بروز رسانی . . .";
+                    lblCurentVer.Text = currentVer;
+                    lblNewVer.Text = newVer.Trim();
+
+                    if (!Directory.Exists(Path.GetTempPath() + "\\Dotnetyar"))
+                    {
+                        Directory.CreateDirectory(Path.GetTempPath() + "\\Dotnetyar");
+                    }
+
+                    int cVer = int.Parse(currentVer.Replace(".", ""));
+                    int nVer = int.Parse(newVer.Trim().Replace(".", ""));
+
+                    _countUpdate = nVer - cVer;
+
+                    _countUpdate++;
+
+                    lblCount.Text = "0" + "/" + _countUpdate;
+
+                    for (int i = 1; i <= _countUpdate; i++)
+                    {
+
+                        WebClient webClient = new WebClient();
+                        webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                        if (i == _countUpdate)
+                            webClient.DownloadFileAsync(new Uri("http://dl.dotnetyar.com/Update/App/App.zip"), Path.GetTempPath() + "\\Dotnetyar\\App.rar");
+                        else
+                        {
+                            string fileName = nVer + ".zip";
+                            webClient.DownloadFileAsync(new Uri("http://dl.dotnetyar.com/Update/Files/" + fileName), Path.GetTempPath() + "\\Dotnetyar\\" + fileName);
+                        }
+
+                        nVer--;
+
+                    }
+                }
+            }
+            else
+                MessageBox.Show("اتصال به اینرتنت را برقرار کنید");
+        }
+
+        private void Completed(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            _count++;
+            lblCount.Text = _count + "/" + _countUpdate;
+            if (_count == _countUpdate)
+            {
+                Thread.Sleep(9000);
+                UpdateApp();
+
+                //اجرای اسکریپت برای بروزرسانی دیتابیس
+                string[] subFiles = Directory.GetFiles(Path.GetTempPath() + "\\Dotnetyar\\Install");
+                SqlConnection ConnectionStr = new SqlConnection(@"Data Source =.; Initial Catalog = DBApp ; Integrated Security=True");
+                foreach (string file in subFiles)
+                {
+                    if (Path.GetExtension(file) == ".sql")
+                    {
+
+                        string script = string.Empty;
+                        using (FileStream s = File.Open(file, FileMode.Open))
+                        {
+                            using (StreamReader st = new StreamReader(s))
+                            {
+                                script = st.ReadToEnd();
+                            }
+                        }
+
+                        Server server = new Server(new ServerConnection(ConnectionStr));
+                        server.ConnectionContext.ExecuteNonQuery(script);
+                        File.Delete(file);
+                    }
+                }
+
+
+                //بررسی موجود بودن فایل برای نصب
+                string[] subFiles2 = Directory.GetFiles(Path.GetTempPath() + "\\Dotnetyar\\Install");
+                if (subFiles2.Length > 0)
+                {
+
+                    button1.Enabled = true;
+                    this.Height = 210;
+                    lblCheck.Text = "بروزرسانی انجام شد.برای تکمیل بروزرسانی لازم است موارد زیر روی سیستم شما " +
+        "نصب گردند.برای نصب هر کدام از ابزارها،آنها را ذخیره و نصب کنید.";
+                }
+                else
+                {
+                    //حذف کلیه محتویات پوشه دات نت یار
+                    DirectoryInfo di = new DirectoryInfo(Path.GetTempPath() + "\\Dotnetyar");
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+
+                    //اجرای نرم افزار اصلی و خروج از برنامه بروزرسانی
+                    System.Diagnostics.Process.Start(Application.StartupPath + "\\App.exe");
+                    Application.Exit();
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] subFiles = Directory.GetFiles(Path.GetTempPath() + "\\Dotnetyar\\Install");
+            FolderBrowserDialog saveFile = new FolderBrowserDialog();
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                //کپی فایل های نیاز به نصب در محل انتخابی کاربر
+                foreach (string file in subFiles)
+                {
+                    string newFullPath = saveFile.SelectedPath + "\\" + Path.GetFileName(file);
+                    File.Move(file, newFullPath);
+                }
+
+                //حذف کلیه محتویات پوشه دات نت یار
+                DirectoryInfo di = new DirectoryInfo(Path.GetTempPath() + "\\Dotnetyar");
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+
+                //اجرای نرم افزار اصلی و خروج از برنامه بروزرسانی
+                System.Diagnostics.Process.Start(Application.StartupPath + "\\App.exe");
+                Application.Exit();
+            }
+
+        }
+
+       
+    }
+}
+ 
